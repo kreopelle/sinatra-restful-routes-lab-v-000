e07ed67d9678c3c1c36830b8f46cd4b39a42ce27
Kayla Reopelle <kreopelle@gmail.com>
2018-07-17 16:47:08 +0000

    automatically backed up by learn

--- db/migrate/20180717164626_create_recipes.rb
+++ db/migrate/20180717164626_create_recipes.rb
@@ -1,4 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.timestamps :cook_time
   end
 end
